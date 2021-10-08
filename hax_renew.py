8ee2ebebad845f013d08da26595bfea744fd6677
misakano2975 <misakano2975@outlook.jp>
2021-10-08 16:12:07 +0000

    设置UA

--- hax_renew.py
+++ hax_renew.py
@@ -8,3 +8,9 @@ import time
 import base64
 import requests
 from bs4 import BeautifulSoup
+
+# 设置UA
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/94.0.4606.61 Safari/537.36 "
+)
